9573a7d6c76277976e21fe2e3310b2dc640f7eaf
Jahongir <azahongir582@gmail.com>
2023-11-15 18:27:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,200 @@
+
+// let ask = prompt("write your name")
+//  if (ask.length % 2 == 0) {
+//     alert("your name are accepted")
+//  } else {
+
+//     ask.length %2 > 0 
+//         alert("your name has been refused")
+//  } 
+
+
+
+
+// let name = "Alex"
+// let ask = prompt("Как вас зовут").trim()
+// let pin = 7777
+// let account = 10000
+
+
+
+//  if (ask.toLowerCase() == name.toLowerCase()) {
+ 
+//     let pass = +prompt("Ваш пин код")
+//     if (pass == pin ) {
+//     let money = +prompt("Сколько хотите обналичить?")
+//     if ( account>=money ) {
+//         account = account - money 
+ 
+//         console.log(account);
+//     } else {
+//         alert ("Не достаточно средств")
+//     }
+
+//     } else {
+//         alert("Не верный пин")
+//     }
+
+//   }  else {
+//     alert ("Пользователь не найден")
+//  }
+
+    
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // NUMBERS 
+// parseInt(num)// "10.55" 10 
+
+// parseFloat(num)// "10.55" 10.55
+
+// // Math.ceil() // округляет число  increase
+// // Math.floor() //  округляет число decrease
+// // Math.round()  округляет число middle
+// // Math.max()
+// // Math.main()
+// // Math.random()
+// // num.toFixed() округляет и переводит в стринг
+// // num.toString() как есть переводит в стринг
+// // +num "55" =>55
+// // .../ remove skobka / operator
+// parseInt makes num and round number
+// parseFloat makes num not round 
+
+
+//  let str = "olya"
+//  console.log(str.indexOf("y"));v  
+
+
+// let ask = confirm("are you 18?")
+
+
+// if (ask==true) {
+//     alert ("приятного просмотра")
+// } else {
+//     alert("you are yet to")
+// } 
+
+// let ask = prompt("how old are you?")
+// if (ask>=18) {
+//     alert ("приятного просмотра")
+// } else {
+//     alert("you are yet to")
+// }
+
+
+// let list = "putin, shavkat, alibek "
+// let name_1 = prompt("what is your name")
+
+// if ( list.includes (name_1.trim()) ) {
+//     alert("enter")
+// } else {
+//     alert("close")
+// }
+
+
+
+
+// let ask =  prompt("write a number")
+// let logo = prompt( "- + / *")
+// let ask_2 = prompt("write second number")
+
+// if ( logo === "-") {
+//     console.log(ask - ask_2);
+// } else if (logo === "+") {
+//     console.log(ask + ask_2);
+// } else if (logo === "/") {
+//     console.log(ask / ask_2);
+// } else if (logo === "*") {
+//     console.log(ask *ask_2);
+// } else {
+//     console.error();
+// }
+
+
+// let  age = +prompt("how old are you")
+// if (age > 1) {
+//     alert("ребенок")
+// } else if ( age >10  ) {
+//     alert("teenager")
+// } else if ( age >20  ) {
+// alert("тебе 30 ")
+// }  else if ( age >30  ) {
+//     alert("тебе 40 ")
+// } else if ( age >40 ) {
+//     alert("тебе 50 ")
+// } else {
+//     console.error("");
+// } 
+
+
+let name = prompt("what is your name") . toUpperCase ()
+ 
+if (name[0] === 'a' &&  name % 2  === 0 ) {
+   let password = +prompt("pass pin")
+    
+
+   
+
+}  else {
+    alert("error")
+}
+
+
+
+
+
+
+
+
+
+
